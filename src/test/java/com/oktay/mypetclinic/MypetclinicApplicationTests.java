de711db5da9d3062d4de6c52de5eb3e52d97dd0f
Oktay Şadoğlu <oktaysadoglu@hotmail.com>
2019-11-24 17:04:48 +0300

    Initial commit

---
+++ src/test/java/com/oktay/mypetclinic/MypetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oktay.mypetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
